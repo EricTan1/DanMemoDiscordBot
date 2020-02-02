40bb60b90d5a6c3dc091b12d49330eb985e329ff
Eric Tan <EricTan1>
2020-02-02 15:29:27 -0500

    added insert method for DB controller

--- DanMemoDiscordBot/DBcontroller.py
+++ DanMemoDiscordBot/DBcontroller.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import inspect
 
 class DBcontroller:
 
@@ -8,10 +9,7 @@ class DBcontroller:
         user="root",
         password="danmemo", port="3306", database="danmemo")
     #print(self.connection)
-    self._mycursor = self.connection.cursor()
-    self._mycursor.execute("SHOW TABLES")
-    # for x in mycursor:
-    # print(x)
+    self._mycursor = self._connection.cursor()
 
   def closeconnection(self):
     ''' (DBcontroller) -> None
@@ -23,7 +21,32 @@ class DBcontroller:
     ''' (DBcontroller, Entity) -> bool
     returns whether or not it is a successful insert
     '''
-    pass
+    # get all the attributes of the members and their corresponding values
+    attributes =inspect.getmembers(entity, lambda a:not(inspect.isroutine(a)))
+    attributes =[a for a in attributes if not(a[0].startswith('__') and a[0].endswith('__'))]
+    
+    attribute_list = []
+    value_list = []
+    valueprep_list=[]
+    
+    for attributetuple in attributes:
+      (attributename, attributevalue) = attributetuple
+      attribute_list.append(attributename)
+      valueprep_list.append('%s')
+      value_list.append(attributevalue)
+    
+    attribute_list= str(tuple(attribute_list)).replace("'","")
+    valueprep_list= str(tuple(value_list)).replace("'","")
+    
+    
+    sql="INSERT INTO {} {} VALUES {}".format(type(entity).__name__,
+                                             attribute_list,valueprep_list)
+    values = tuple(value_list)
+    self._mycursor.execute(sql,values)
+    self._connection.commit()
+    print(self._mycursor.rowcount, "record inserted.")
+    
+    
   
   def updateData(self, entity, columns, values):
     ''' (DBcontroller, Entity, str, ?) -> bool
--- Entities/Character.py
+++ Entities/Character.py
@@ -1,14 +1,17 @@
+import inspect
+
 class Character:
     ''' This class is an object that represents the
     Character table in the DB
     '''
-    def __init__(self, name, iscollab):
+    def __init__(self, characterid,name, iscollab):
         ''' (self, str, bool) -> Character
         name : the name of the character (usually found in the characterlist
                                           section)
         isCollab : is the character originally from danmachi universe?
         '''
         self.name = name
+        self.characterid= characterid
         self.iscollab = iscollab
 
     def __str__(self):
@@ -35,4 +38,4 @@ class Stats:
         self.value = value
 
     def __str__(self):
-        return self.name    
+        return self.name
