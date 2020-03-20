b0c7f03e39ac1787293e2e740a2a10bb8145ee77
Eric Tan <EricTan1>
2020-03-20 14:14:58 -0400

    fixed sql error regarding speed table

--- DanMemoDiscordBot/DBcontroller.py
+++ DanMemoDiscordBot/DBcontroller.py
@@ -204,7 +204,8 @@ class DBcontroller:
   def assembleAdventurerSkill(self, skillid):
     ret =""
     skill_sql="SELECT skilltype, skillname FROM danmemo.adventurerskill where adventurerskillid={}".replace("danmemo",self.database).format(skillid)
-    effects_sql="SELECT  t.name,m.value,a.name,e.duration,ty.name,ele.name,s.name FROM danmemo.adventurerskilleffects as e,danmemo.target as t,danmemo.modifier as m,danmemo.attribute as a, danmemo.type as ty, danmemo.element as ele, danmemo.speed as s where adventurerskillid={} and m.modifierid=e.modifierid and e.targetid = t.targetid and a.attributeid = e.attributeid and e.eleid=ele.elementid and ty.typeid=e.typeid and s.speedid = e.speedid".replace("danmemo",self.database).format(skillid)
+    effects_sql="SELECT  t.name,m.value,a.name,e.duration,ty.name,ele.name,s.name FROM (danmemo.adventurerskilleffects as e,danmemo.target as t,danmemo.modifier as m,danmemo.attribute as a, danmemo.type as ty, danmemo.element as ele) LEFT JOIN danmemo.speed as s ON s.speedid = e.speedid where adventurerskillid={} and m.modifierid=e.modifierid and e.targetid = t.targetid and a.attributeid = e.attributeid and e.eleid=ele.elementid and ty.typeid=e.typeid".replace("danmemo",self.database).format(skillid)
+    print(effects_sql)
     self._mycursor.execute(skill_sql)
     for row in self._mycursor:
       # skilltype : skillname
