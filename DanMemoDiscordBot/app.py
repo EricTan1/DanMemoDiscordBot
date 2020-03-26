c64b9a7a0ff309cea7ecfad3a417cd2c3866770f
Eric Tan <EricTan1>
2020-03-25 20:54:14 -0400

    help command implemented

--- DanMemoDiscordBot/app.py
+++ DanMemoDiscordBot/app.py
@@ -163,6 +163,32 @@ async def skillSearch(ctx, *search):
         await ctx.send(embed=temp_embed)        
     db.closeconnection()
 
+@client.command(aliases=['h','command','commands'])
+async def help(ctx):
+    temp_embed = discord.Embed()
+    temp_embed.color = 3066993
+    temp_embed.title = "Command List"
+    with open('./help.txt') as fp:
+        line = fp.readline()
+        print(line.strip())
+        
+        while(line):
+            print(line.strip())
+            if(line.strip() =="F"):
+                line = fp.readline()
+                name = line
+                value = ""
+                line = fp.readline()
+                while line.strip() !="F" and line.strip() !="E":
+                    value = value + line
+                    line = fp.readline()
+                temp_embed.add_field(name=name, value=value, inline=False)
+            elif(line.strip()=="E"):
+                break;
+        temp_embed.set_thumbnail(url="https://static.myfigurecollection.net/pics/figure/large/571996.jpg")
+    await ctx.author.send(embed=temp_embed)
+
+
 async def skillSearchRotatingPage(ctx, search, page_list, my_set):
     print(page_list)
     # set up
