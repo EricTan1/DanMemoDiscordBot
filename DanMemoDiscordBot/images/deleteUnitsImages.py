3a13781cec0a676ab034c883f3ef16ece8d57c8d
Youness Skalli <yon.skalli@gmail.com>
2020-09-11 15:14:24 +0200

    Sparkling Summer Carnival datamine (Thanks Bob ^^)

--- DanMemoDiscordBot/images/deleteUnitsImages.py
+++ DanMemoDiscordBot/images/deleteUnitsImages.py
@@ -8,4 +8,6 @@ path = "units/"
 for folders in os.listdir(path):
     for filename in os.listdir(path+"/"+folders):
         if filename not in wanted:
+        	file = path+"/"+folders+"/"+filename
+        	print("Deleting "+file)
         	os.remove(path+"/"+folders+"/"+filename)
