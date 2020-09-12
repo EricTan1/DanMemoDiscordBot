99de2e202bdbcd44fbb108968da94a68c6e20f09
Youness Skalli <yon.skalli@gmail.com>
2020-09-12 22:02:07 +0200

    Added free Amid

--- DanMemoDiscordBot/images/deleteUnitsImages.py
+++ DanMemoDiscordBot/images/deleteUnitsImages.py
@@ -4,7 +4,7 @@ import sys
 
 wanted = ["all_rectangle.png","hex.png","texture.png","white.png"]
 
-path = "units/"
+path = "units"
 for folders in os.listdir(path):
     for filename in os.listdir(path+"/"+folders):
         if filename not in wanted:
