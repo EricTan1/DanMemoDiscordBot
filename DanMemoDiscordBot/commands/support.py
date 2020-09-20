279ba32be80434dd2d4f31e2ff8615992d6652b6
EricTan1 <erictanorange1999@gmail.com>
2020-09-19 23:09:30 -0400

    updating support with patreon

--- DanMemoDiscordBot/commands/support.py
+++ DanMemoDiscordBot/commands/support.py
@@ -4,6 +4,6 @@ from commands.utils import Status
 async def run(ctx):
     embed = discord.Embed()
     embed.color = Status.KO.value
-    embed.title = "Use this link to join our support server!"
-    embed.description = "https://discord.gg/b8xGHhm"
+    embed.title = "Use this link to join our support server!\n Also check out our patreon!"
+    embed.description = "Support Server: https://discord.gg/b8xGHhm \nPatreon:https://www.patreon.com/aisbot"
     await ctx.send(embed=embed)
