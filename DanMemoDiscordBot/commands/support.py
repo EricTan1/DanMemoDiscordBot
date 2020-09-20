edd71d95725d17ca02ff11a352ff47fa319d5491
EricTan1 <erictanorange1999@gmail.com>
2020-09-19 23:16:51 -0400

    fix formatting for support

--- DanMemoDiscordBot/commands/support.py
+++ DanMemoDiscordBot/commands/support.py
@@ -4,6 +4,6 @@ from commands.utils import Status
 async def run(ctx):
     embed = discord.Embed()
     embed.color = Status.KO.value
-    embed.title = "Use this link to join our support server!\n Also check out our patreon!"
-    embed.description = "Support Server: https://discord.gg/b8xGHhm \nPatreon:https://www.patreon.com/aisbot"
+    embed.title = "Use this link to join our support server!\nAlso check out our patreon!"
+    embed.description = "Support Server: https://discord.gg/b8xGHhm \nPatreon: https://www.patreon.com/aisbot"
     await ctx.send(embed=embed)
