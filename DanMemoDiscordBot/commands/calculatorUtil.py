ff768f2f07964b5c102cbb2541dd99b9cd64cd8a
Jannick Kremer <jannick-kremer@gmx.de>
2023-07-03 20:47:30 +0200

    Fix buff/debuff removal skills for multi-word attributes

--- DanMemoDiscordBot/commands/calculatorUtil.py
+++ DanMemoDiscordBot/commands/calculatorUtil.py
@@ -808,7 +808,7 @@ def interpretSkillAdventurerEffects(
         elif "removal_no_assist" in curr_attribute:
             is_buff = not ("debuff" in curr_attribute)
 
-            temp_list = curr_attribute.replace("removal_no_assist", "").split("_")
+            temp_list = curr_attribute.replace("_removal_no_assist", "").split("_")
             try:
                 temp_list.remove("buff")
             except:
@@ -817,7 +817,7 @@ def interpretSkillAdventurerEffects(
                 temp_list.remove("debuff")
             except:
                 pass
-            temp_attribute = " ".join(temp_list).strip()
+            temp_attribute = "_".join(temp_list).strip()
             if current_target in ["self", "allies"]:
                 for curr_adv in target_list:
                     curr_adv.pop_boostCheckAdv(is_buff, temp_attribute)
